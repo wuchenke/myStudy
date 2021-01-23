0acb922135f037a5a4639351c16cccb550b3e01f
wuchenke <643707270@qq.com>
2021-01-23 22:54:46 +0800

    [修改]新增注释

--- function/src/main/java/codetest/CompareTest.java
+++ function/src/main/java/codetest/CompareTest.java
@@ -3,7 +3,8 @@ package codetest;
 
 /**
  * @author 64370
- * @Description
+ * @Description 字符串拼接测试，使用 String + 的方式拼接字符串，每次都会生成一个 StringBuilder 对象，并调用 StringBuilder
+ * 对象的 append 方法，最后以 toString 的方式返回。
  * @Date 2021/1/19
  */
 public class CompareTest {
@@ -16,5 +17,6 @@ public class CompareTest {
             sb.append(i);
         }
         System.out.println(sb);
+
     }
 }
